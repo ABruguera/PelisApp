9ff9edd1658204c96eb0c91d26293e8ce201251e
ABruguera <brugueracalvo@gmail.com>
2021-10-16 21:25:31 +0200

    Empezada pantalla de detalles de película

---
+++ src/app/modules/movies/movie-details/movie-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Observable } from "rxjs";
+import { Movie } from "../state/movie.model";
+import { MoviesQuery } from "../state/movies.query";
+import { MoviesService } from "../state/movies.service";
+
+@Component({
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.component.html",
+  styleUrls: ["./movie-details.component.scss"],
+})
+export class MovieDetailsComponent implements OnInit {
+  movie$!: Observable<Movie | undefined>;
+  constructor(private moviesServices: MoviesService, private moviesQuery: MoviesQuery, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.moviesServices.getAllMovies().subscribe();
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = params.get("id");
+      if (id) {
+        this.movie$ = this.moviesQuery.selectEntity(Number(id));
+      }
+    });
+  }
+}
