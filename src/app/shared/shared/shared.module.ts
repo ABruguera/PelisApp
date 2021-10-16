d0548fa407efdb99af2438433c9c9c17ab287bb6
ABruguera <brugueracalvo@gmail.com>
2021-10-16 19:58:13 +0200

    Componente loading, app container altura 100vh

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatsharedModule } from "../matshared/matshared.module";
 import { TranslateModule } from "@ngx-translate/core";
+import { LoadingComponent } from "src/app/components/loading/loading.component";
 
 @NgModule({
-  declarations: [],
-  exports: [CommonModule, MatsharedModule, TranslateModule],
+  declarations: [LoadingComponent],
+  imports: [CommonModule, MatsharedModule],
+  exports: [CommonModule, MatsharedModule, TranslateModule, LoadingComponent],
 })
 export class SharedModule {}
