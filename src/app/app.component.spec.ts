8ed3f6f44fb17baa81b63178cf933783ca8f9648
ABruguera <brugueracalvo@gmail.com>
2021-10-20 21:13:59 +0200

    Arreglados imports de tests de header y app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from "@angular/core/testing";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TranslateModule } from "@ngx-translate/core";
 import { AppComponent } from "./app.component";
@@ -8,7 +12,15 @@ import { HeaderComponent } from "./components/header/header.component";
 describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TranslateModule.forRoot(), MatToolbarModule],
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [AppComponent, HeaderComponent],
     }).compileComponents();
   });
